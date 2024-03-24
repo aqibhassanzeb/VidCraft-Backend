18a0275d3e64b57086f57d6282da13aee7d6d976
Uranium6 <134263714+Uranium6@users.noreply.github.com>
2024-03-24 18:18:40 +0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ app.use('/api', routes);
 createOutputFolder()
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Mediacraft Backend API);
+  res.send('Mediacraft Backend APIs);
 });
 
 app.listen(port, () => {
